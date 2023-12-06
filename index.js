0b34c993f64c1c30bba1e995e7f19fa4a93a4be1
Mohamed Brzan <mohamedbrzan11@gmail.com>
2023-12-06 14:13:55 +0200

    Explain how's git recording changes in repository

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 //* add specific extensions
 //? git add *.js
+
+//* Recording changes to the Repository
+//? Untracked => if you don't make any changes since last commit
+//? Ummodified => if you don't make any changes since last commit
+//? Modified => if you make any edit to the file after last commit
+//? Staged => when you make git add to those files becomes staged
