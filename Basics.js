394a303dbeea3f10c28643385aac5e9dcb4f6f7c
Mohamed Brzan <mohamedbrzan11@gmail.com>
2023-12-23 22:43:44 +0200

    learnt the meaning of ( U - M - S ) letters

--- Basics.js
+++ Basics.js
@@ -13,3 +13,9 @@
 
 //* Cloning an Existing Repository In A Specific Directory
 //TODO: ** git clone https://github.com/[Account Username]/[File Name].git [Directory Folder Name] **
+
+//* Recording changes to the Repository
+//? Untracked => if you don't make any changes since last commit
+//? Ummodified => if you don't make any changes since last commit
+//? Modified => if you make any edit to the file after last commit
+//? Staged => when you make git add to those files after make changes
--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 
 
-//* Recording changes to the Repository
-//? Untracked => if you don't make any changes since last commit
-//? Ummodified => if you don't make any changes since last commit
-//? Modified => if you make any edit to the file after last commit
-//? Staged => when you make git add to those files after make changes
 
 //* any thing to commitfdsgfsdgf
