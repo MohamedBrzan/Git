deccd95a79f3dbb2356f77a75bf392cffbfabc7d
Mohamed Brzan <mohamedbrzan11@gmail.com>
2023-12-23 22:41:53 +0200

    Understanding how to add a specific extentions

---
+++ Basics.js
@@ -0,0 +1,15 @@
+//* Initializing a Repository in an Existing Directory
+//TODO:  ** git init **
+
+//* add specific extensions
+//TODO: ** git add *.js **
+
+//* Cloning an Existing Repository
+//? Https Protocol
+//TODO: ** git clone https://github.com/[Account Name]/[File Name].git **
+
+//? Server Protocol
+//TODO: ** git clone git:// or user@server:path/to/repo.git **
+
+//* Cloning an Existing Repository In A Specific Directory
+//TODO: ** git clone https://github.com/[Account Username]/[File Name].git [Directory Folder Name] **
--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-//* add specific extensions
-//? git add *.js
+
 
 //* Recording changes to the Repository
 //? Untracked => if you don't make any changes since last commit
@@ -7,4 +6,4 @@
 //? Modified => if you make any edit to the file after last commit
 //? Staged => when you make git add to those files after make changes
 
-//* any thing to commit
+//* any thing to commitfdsgfsdgf
