a1c804c41bf7ef5c666729a01bb62e522661516d
MohamedMahmoud <83614946+MohamedBrzan@users.noreply.github.com>
2023-12-27 02:53:04 +0200

    learnt the commit set and how to connect with the tree and how's the tree connect with the blob object

---
+++ Branching.js
@@ -0,0 +1,9 @@
+//* Git Branching
+
+//* Branches in a Nutshell
+
+//? each commit has a pointer to that root tree and all commit metadata ( metadata like author name, commit message etc.. )
+//? each commit has a tree object ( to lists the contents of the directory and specifies which file names are stored as which blobs )
+//? each file has blob object ( to representing it's content )
+
+
